2233c334a2ae02c1e5198419bef8cb8b9c7ddcde
Invazing <invazing@hotmail.com>
2021-07-29 13:58:06 -0300

    Adicionar .gitignore e .gitattributes.

---
+++ Encapsulamento e Propriedades/Pessoa.cs
@@ -0,0 +1,62 @@
+﻿public class Pessoa
+{
+    // Precisa ser a privada 
+    private string nome;
+    private string genero;
+    private int idade;
+    private string cor;
+
+   
+    public Pessoa(string nome)
+    {
+        this.Nome = nome;
+    }
+    /* precisa ser publica com metodos get e set (Deixando a inicial Maiuscula)
+     * get { return nome;}
+     * set { nome = value;}
+    */
+    public string Cor
+    {
+        set
+        {
+            if (value == "Amarelo")
+            {
+                System.Console.WriteLine("Voce n pode escolher Amarelo");
+                           
+            }
+            else
+            {
+                cor = value;
+            }
+            
+        }
+        get { return cor; }
+    }
+    
+public string Nome { get; }
+
+    public string Genero
+    {
+        set
+        {
+            genero = value;
+            System.Console.WriteLine(genero);
+        }
+    }
+
+    public int Idade
+    {
+        set
+        {
+            if (value < 18)
+            {
+                System.Console.WriteLine("Idade tem que ser maior que 18");
+            }
+            else
+            {
+                this.idade = value;
+            }
+        }
+        get { return idade; }
+    }
+}
---
+++ Encapsulamento e Propriedades/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Encapsulamento_e_Propriedades
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa("Maria");
+            p1.Genero = "masculino";
+            p1.Idade = 19;
+            p1.Cor = "Rosa";
+
+            Console.WriteLine(p1.Nome);
+            Console.WriteLine(p1.Idade);
+            Console.WriteLine(p1.Cor);
+        }
+    }
+
+
+
+
+   
+}
